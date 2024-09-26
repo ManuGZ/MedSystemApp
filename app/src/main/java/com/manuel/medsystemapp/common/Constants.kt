a9e5e17bd8bbd94f3e78ce1d9327903037060ba9
ManuGZ <galonso097@gmail.com>
2024-09-25 22:22:30 -0500

    feat: add api link

--- app/src/main/java/com/manuel/medsystemapp/common/Constants.kt
+++ app/src/main/java/com/manuel/medsystemapp/common/Constants.kt
@@ -1,8 +1,6 @@
 package com.manuel.medsystemapp.common
 
 object Constants {
-    /*
-        Debe terminar con un /
-     */
-    const val BASE_URL = "https://dev.formandocodigo.com/ServicioTour/"
+
+    const val BASE_URL = "https://medsolutions.up.railway.app/swagger-ui/index.html#/"
 }
