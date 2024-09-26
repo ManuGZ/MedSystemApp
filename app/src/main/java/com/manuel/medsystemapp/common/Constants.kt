f82d9b1d7c9c43d5a34102d04aaf9632a2967b02
ManuGZ <galonso097@gmail.com>
2024-09-25 22:00:51 -0500

    chore: plugins added, and basic resources

---
+++ app/src/main/java/com/manuel/medsystemapp/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.manuel.medsystemapp.common
+
+object Constants {
+    /*
+        Debe terminar con un /
+     */
+    const val BASE_URL = "https://dev.formandocodigo.com/ServicioTour/"
+}
