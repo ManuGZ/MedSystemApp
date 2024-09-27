769294466646228b0d14c520daebe1c94d907641
JuanMontes18 <u202210775@upc.edu.pe>
2024-09-26 23:39:20 -0500

    feat: create of the post function

--- app/src/main/java/com/manuel/medsystemapp/data/repository/AppointmentRepository.kt
+++ app/src/main/java/com/manuel/medsystemapp/data/repository/AppointmentRepository.kt
@@ -27,4 +27,26 @@ class AppointmentRepository(private val service: AppointmentService) {
             return@withContext Resource.Error(message = e.message ?: "An exception occurred.")
         }
     }
+
+    suspend fun postAppointments(appointment: Appointment): Resource<Appointment> = withContext(Dispatchers.IO){
+        try {
+            //val response = service.createAppointment(appointment.toAppointmentDto())
+            val appointmentDto = AppointmentDto(
+                appointment.id,
+                appointment.doctorId,
+                appointment.patientId,
+                appointment.date,
+                appointment.reason
+            )
+            val response = service.postAppointment(appointmentDto)
+            if (response.isSuccessful) {
+                response.body()?.let { postAppointmentDto: AppointmentDto ->
+                    return@withContext Resource.Success(data = appointmentDto.toAppointment())
+                }
+            }
+            return@withContext Resource.Error(message = response.message())
+        } catch (e: Exception) {
+            return@withContext Resource.Error(message = e.message ?: "An exception occurred.")
+        }
+    }
 }
